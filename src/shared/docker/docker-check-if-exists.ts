6b074d350c52df699e6f5ff40a66ac6aa92b367e
Kerren Ortlepp <kerren@entrostat.com>
2023-06-24 21:28:42 +0200

    feat(exist-check): Use regctl to check if a manifest exists or not to ensure that we don't get the unsupported manifest issues

--- src/shared/docker/docker-check-if-exists.ts
+++ src/shared/docker/docker-check-if-exists.ts
@@ -1,15 +1,19 @@
 import { DockerBuildOptions } from "../../models/docker-build-options";
 import { generateImageName } from "./generate-image-name";
 import { executeCommand } from "../execute-command";
+import { installRegctl } from "../install-regctl";
 
 export async function dockerCheckIfExists(
   dockerBuildOptions: DockerBuildOptions,
   hash: string,
 ) {
   console.log("Checking if image and hash already exists");
+  const regctlPath = await installRegctl();
   const imageName = generateImageName(dockerBuildOptions, hash);
-  const result = await executeCommand(
-    `docker manifest inspect ${imageName} > /dev/null ; echo $?`,
-  );
-  return result.trim() === "0";
+  try {
+    await executeCommand(`${regctlPath} manifest head ${imageName}`);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
